7b2e2ae309ce328290ec4797cd57737b6a30c66b
Afaaf Hariri <afaafhariri@icloud.com>
2025-07-28 00:23:20 +0530

    unwanted comments

--- main.py
+++ main.py
@@ -4,22 +4,9 @@ import os
 
 app = FastAPI()
 
-# @app.on_event("startup")
-# async def list_routes():
-#     print("Available routes:")
-#     for route in app.router.routes:
-#         print(route.path, route.methods)
-
 @app.get("/")
 async def root():
     return {"status": "ok"}
-
-# app.post("/upload")
-# async def upload_cv(file: UploadFile = File(...)):
-#      return JSONResponse({
-#         "filename": file.filename,
-#         "content_type": file.content_type
-#     })
 
 @app.post("/upload")
 async def upload_cv(file: UploadFile = File(...)):
